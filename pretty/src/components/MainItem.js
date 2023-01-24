11e15293858c8e4fc865bf00c8df9c68e8f8fc12
panchalleena1286 <panchalleena8839@gmail.com>
2023-01-24 20:33:32 +0530

    initial commit

--- pretty/src/components/MainItem.js
+++ pretty/src/components/MainItem.js
@@ -2,41 +2,41 @@ export const MainItem = [
     {
         title: "HOME",
         url: "/home",
-        cName: "nav-links"
+        cName: "nav-links1"
       },
       {
         title: "MAKEUP",
         url: "/buy/makeup",
-        cName: "nav-links"
+        cName: "nav-links1"
       },
       {
         title: "HAIRCARE",
         url: "/buy/haircare",
-        cName: "nav-links"
+        cName: "nav-links1"
       },
       {
         title: "SKINCARE",
         url: "/buy/skincare",
-        cName: "nav-links"
+        cName: "nav-links1"
       },
       {
         title: "COLLECTION",
         url: "/collection",
-        cName: "nav-links"
+        cName: "nav-links1"
       },
       {
         title: "REWARDS",
         url: "/reward",
-        cName: "nav-links"
+        cName: "nav-links1"
       },
       {
         title: "PRETTY STUDIO",
         url: "/studio",
-        cName: "nav-links"
+        cName: "nav-links1"
       },
       {
         title: "OFFERS",
         url: "/offer",
-        cName: "nav-links"
+        cName: "nav-links1"
       },
 ]
